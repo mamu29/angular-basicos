6fd087b108423ff48e6118417278a46d30460531
Emanuel Romero <emanuelromero80@gmail.com>
2021-12-08 13:43:04 -0300

    segundo commit

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './main-page/main-page.component';
 
+import { MainPageComponent } from './main-page/main-page.component';
+import { FormsModule } from '@angular/forms';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    PersonajesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  exports: [
+  exports: [    
     MainPageComponent
   ]
 })
